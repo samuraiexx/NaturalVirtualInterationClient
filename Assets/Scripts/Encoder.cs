3354370108b00663f23a5f005b2d98d6216e273d
Mateus de Castro <samuraiexx@gmail.com>
2020-06-28 05:54:42 -0300

    Bit Brushing

--- Assets/Scripts/Encoder.cs
+++ Assets/Scripts/Encoder.cs
@@ -2,17 +2,16 @@
 using UnityEngine;
 using System.Runtime.InteropServices;
 
-
 public class Encoder {
   private int width, height;
   private static Encoder encoder;
-  private Byte[] yv12DataBuffer, encodedDataBuffer;
+  private Byte[] encodedDataBuffer, rgbDataBuffer;
 
   [DllImport("NaturalVirtualInteractionVideoEncoder")]
   extern static int vpx_init(int width, int height);
 
   [DllImport("NaturalVirtualInteractionVideoEncoder")]
-  extern static int vpx_encode(byte[] yv_frame, byte[] encoded, bool force_key_frame);
+  extern static int vpx_encode(byte[] rgb_frame, byte[] encoded, bool force_key_frame);
 
   [DllImport("NaturalVirtualInteractionVideoEncoder")]
   extern static void vpx_cleanup();
@@ -26,28 +25,26 @@ public class Encoder {
   }
 
   public byte[] processFrame(Color32[] frame) {
-    // DateTime startTime;
-    // double deltaTime;
+    // DateTime startTime = DateTime.Now;
 
     int encodedSize;
-    //startTime = DateTime.Now;
-    convertRGB2YUV12(frame, yv12DataBuffer); // TODO: Improve time
 
-    // deltaTime = (DateTime.Now - startTime).TotalSeconds;
-    // Debug.Log($"YUV Convert deltaTime: {deltaTime}");
-    // startTime = DateTime.Now;
+    for (int i = 0; i < height; i++) {
+      int lineOffset = 3 * width * height - 3 * width * (i + 1);
+      for (int j = 0; j < width; j++) {
+        rgbDataBuffer[lineOffset + 3 * j] = frame[width * i + j].r;
+        rgbDataBuffer[lineOffset + 3 * j + 1] = frame[width * i + j].g;
+        rgbDataBuffer[lineOffset + 3 * j + 2] = frame[width * i + j].b;
+      }
+    }
 
-    encodedSize = vpx_encode(yv12DataBuffer, encodedDataBuffer, false);
-
-    // deltaTime = (DateTime.Now - startTime).TotalSeconds;
-    // Debug.Log($"Convert deltaTime: {deltaTime}");
-    //startTime = DateTime.Now;
+    encodedSize = vpx_encode(rgbDataBuffer, encodedDataBuffer, false);
 
     Byte[] encodedData = new byte[encodedSize];
     Array.Copy(encodedDataBuffer, 0, encodedData, 0, encodedSize);
 
-    //deltaTime = (DateTime.Now - startTime).TotalSeconds;
-    //Debug.Log($"Copy array deltaTime: {deltaTime}");
+    // double deltaTime = (DateTime.Now - startTime).TotalSeconds;
+    // Debug.Log($"Copy array deltaTime: {deltaTime}");
 
     return encodedData;
   }
@@ -55,39 +52,12 @@ public class Encoder {
   private Encoder(int width, int height) {
     this.width = width;
     this.height = height;
-    yv12DataBuffer = new Byte[32 * width * height];
     encodedDataBuffer = new Byte[32 * width * height];
+    rgbDataBuffer = new Byte[3 * width * height];
     vpx_init(width, height);
   }
 
   ~Encoder() {
     vpx_cleanup();
   }
-
-  private void convertRGB2YUV12(Color32[] input, byte[] output) // It also flips the image vertically
-  {
-    int size = width * height;
-
-    for (int i = 0; i < height; i++) {
-      for (int j = 0; j < width; j++) {
-        double R = (input[i * width + j].r);
-        double G = (input[i * width + j].g);
-        double B = (input[i * width + j].b);
-
-        output[size - 1 - (width * i + j)] = Convert.ToByte(R * .299000 + G * .587000 + B * .114000); // Y
-      }
-    }
-
-    for (int i = 0; i < height / 2; i++) {
-      for (int j = 0; j < width / 2; j++) {
-        double R = ((input[2 * i * width + 2 * j].r) + (input[(2 * i + 1) * width + 2 * j].r) + (input[2 * i * width + 2 * j + 1].r) + (input[(2 * i + 1) * width + 2 * j + 1].r)) / 4.0;
-        double G = ((input[2 * i * width + 2 * j].g) + (input[(2 * i + 1) * width + 2 * j].g) + (input[2 * i * width + 2 * j + 1].g) + (input[(2 * i + 1) * width + 2 * j + 1].g)) / 4.0;
-        double B = ((input[2 * i * width + 2 * j].b) + (input[(2 * i + 1) * width + 2 * j].b) + (input[2 * i * width + 2 * j + 1].b) + (input[(2 * i + 1) * width + 2 * j + 1].b)) / 4.0;
-
-        output[5 * size / 4 - 1 - (i * width / 2 + j)] = Convert.ToByte(R * .500000 + G * -.418688 + B * -.081312 + 128); // V
-        output[6 * size / 4 - 1 - (i * width / 2 + j)] = Convert.ToByte(R * -.168736 + G * -.331264 + B * .500000 + 128); // U
-      }
-    }
-  }
-
 }
