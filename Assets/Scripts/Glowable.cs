d321a765c0eeecdd9ffcbec59fcafdd4cb12339b
Xavier <joaopedroaxavier@gmail.com>
2020-07-02 19:02:33 -0300

    First scenario for demo

---
+++ Assets/Scripts/Glowable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glowable : MonoBehaviour {
+
+    private enum State {
+        NotGlowing,
+        Glowing
+    }
+
+    State state;
+
+    public Material defaultMaterial;
+    public Material glowingMaterial;
+
+    // Start is called before the first frame update
+    void Start() {
+        state = State.NotGlowing;
+    }
+
+    // Update is called once per frame
+    void Update() {}
+
+    public void Glow() {
+        if (state == State.NotGlowing) {
+            gameObject.GetComponent<MeshRenderer>().material = glowingMaterial;
+            state = State.Glowing;
+        }
+    }
+
+    public void Unglow() {
+        if (state == State.Glowing) {
+            gameObject.GetComponent<MeshRenderer>().material = defaultMaterial;
+            state = State.NotGlowing;
+        }
+    }
+}
